bf4c54ace6be921f964d4c65514c6c061d06c12c
Bret Copeland <bret@atlantisflight.org>
2014-08-31 23:10:25 -0400

    test: Stick migrator in tests setup function.

--- test/init.js
+++ test/init.js
@@ -10,9 +10,23 @@ var Sql = require('../src/node_modules/Sql');
 /*
 	setup() and teardown() will be run for each test in each suite
  */
-setup(function ()
+setup(function * ()
 {
 	console.log('hello there');
+
+	var Config = require('../src/node_modules/Config');
+	yield Config.initialize();
+
+	var Path = require('path');
+	var PgMayflower = require('pg.mayflower');
+
+	// run SQL migrations
+	var migrator = new PgMayflower({
+		directory: Path.resolve(__dirname, '../migrations'),
+		connectionString: Config.sql.testingConnection
+	});
+	console.log('Migrating ' + Config.sql.testingConnection);
+	yield migrator.migrateAll();
 });
 
 teardown(function ()
