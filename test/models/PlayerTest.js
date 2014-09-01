40ad83b63212b7698227a2847c17ca3c400e5aa8
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2014-09-01 17:01:06 -0400

    player: rename .get() to .getById(); add .getByEmail(); ensure emails are inserted into the db lowercase; allow email-only insertions, setting name to email

--- test/models/PlayerTest.js
+++ test/models/PlayerTest.js
@@ -10,21 +10,38 @@ suite('Player tests', function ()
 		var p = { name: 'Jarrod' };
 		var id = yield Player.insert(p);
 		assert(id >= 0, 'player was not inserted');
+		
+		// can also insert a player by just email
+		p = { email: 'JARROD@gmail.com' };
+		id = yield Player.insert(p);
+		assert(id >= 0, 'player was not inserted');
+		
+		p = yield Player.getById(id);
+		assert(p.email === 'jarrod@gmail.com', 'email should have been normalized');
+		assert(p.name === 'jarrod@gmail.com', 'name should be email');
+		
 	});
 	
-	test('getting a player', function* () 
+	test('getting a player by id', function* () 
 	{
-		var p = yield Player.get(0);
+		var p = yield Player.getById(0);
 		assert(p == null, 'there should be no player with id == 0');
 		
 		var newP = { name: 'Billy Mays', rating: 100 };
 		var id = yield Player.insert(newP);
-		console.log(id);
 		
-		// issue here is the db has been rolled back after Player.insert completes
-		p = yield Player.get(id);
+		p = yield Player.getById(id);
 		assert(p != null, 'should have gotten a player');
 		assert(p.name === newP.name, 'names should match');
 		assert(p.rating == newP.rating, 'ratings should match');
+	});
+
+	test('getting a player by email', function* ()
+	{
+		// we normalized emails to lower case
+		yield Player.insert({ email: 'TEST@TEST.COM' });
+		
+		var p = yield Player.getByEmail('test@test.com'); 
+		assert(p != null, 'should have gotten a player');
 	});
 });
