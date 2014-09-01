f32be3b3c8db4a803ef782bc525e3cf751f64a4f
Amber Tunnell <amber.tunnell@gmail.com>
2014-08-31 21:13:45 -0400

    adds chosen jquery files and changes new games form

--- src/web/static/js/games.js
+++ src/web/static/js/games.js
@@ -1,15 +1,5 @@
 $(function(){
 
-  $("#singles").change(function () {
-    console.log("Singles selected.");
-    $('.input-form').empty();
-    $('.input-form').append("<br><input type='text' name='player1[name]' placeholder='player 1'><br><input type='text', name='player1[score]' placeholder='player 1 score'><br><br><input type='text', name='player2[name]' placeholder='player 2'><br><input type='text', name='player2[score]' placeholder='player 2 score'><br><br><input type='submit' class='btn'>");
-  });
-
-  $("#doubles").change(function () {
-    console.log("Doubles selected.");
-    $('.input-form').empty();
-    $('.input-form').append("<br><h2>team 1</h2><input type='text' name='team1[player1]' placeholder='player 1'><input type='text', name='team1[player2]' placeholder='player 2'><br><br><input type='text', name='team1[score]' placeholder='team 1 score'><br><h2>team 2</h2><input type='text' name='team2[player3]' placeholder='player 3'><input type='text', name='team2[player4]' placeholder='player 4'><br><br><input type='text', name='team2[score]' placeholder='team 2 score'><br><br><input type='submit' class='btn'>");
-  });
+ $(".chosen-select").chosen({no_results_text: "Nobody found!"}); 
 
 });
