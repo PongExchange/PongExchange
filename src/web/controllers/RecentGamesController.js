0a77be75c14081ccbdde957534bd4fa11fb56ad2
gio <gio@isdev.net>
2014-09-01 00:54:21 -0400

    added very basic recent games functionalty; could not get db query to work yet.  Not sure if wired properly with index.js

---
+++ src/web/controllers/RecentGamesController.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(function(exports) {
+    var debug = require('neo-debug')('web.HomeController');
+    var recentgames = require('RecentGames');
+
+    function * indexGET() {
+        var recentGames = yield recentgames.getRecentGames();
+        var model = { recentGames: recentGames }
+        yield this.render('recentgames/index', model);
+    }
+
+    //function * allGET() {
+    //    var allGames = yield recentgames.getAllGames();
+    //    var model = { games: allGames };
+    //    yield this.render('recentgames/all', model);
+    //}
+
+    exports.indexGET = indexGET;
+    //exports.allGET = allGET;
+})(module.exports);
