d2fdf19d263278ad22d755fa10e07e91636e13bb
Jarrod Dixon <jarrod.m.dixon@gmail.com>
2014-09-28 02:09:13 -0400

    matches: matches should be complete after one team wins at least 2 games

--- test/models/MatchTest.js
+++ test/models/MatchTest.js
@@ -10,7 +10,12 @@ suite('Match tests', function()
 {
 	test('playing a singles match', function*()
 	{
-		var g = yield TestHelpers.getGame(),
+		var gOptions = {
+			team1: [yield TestHelpers.getPlayer()],
+			team2: [yield TestHelpers.getPlayer()]
+		};
+		
+		var g = yield TestHelpers.getGame(gOptions),
 			m = yield Match.newMatch();
 		
 		assert(m.id > 0, 'match should have been inserted');
@@ -21,8 +26,23 @@ suite('Match tests', function()
 		var gLoaded = yield Game.getById(g.id);
 		assert.deepEqual(gLoaded, g, 'persisted game should match its original');
 		
-		var gamesByMatches = yield Game.getAllByMatchId(m.id);
-		assert.equal(gamesByMatches.length, 1);
-		assert.deepEqual(gamesByMatches[0], g);
+		var games = yield Game.getAllByMatchId(m.id);
+		assert.equal(games.length, 1);
+		assert.deepEqual(games[0], g);
+		
+		games = yield m.getAllGames();
+		assert.deepEqual(games[0], g);
+		
+		var isComplete = yield m.isComplete();
+		assert(isComplete === false, 'match must have a minimum of 2 matches to be complete');
+		
+		g = yield TestHelpers.getGame(gOptions);
+		yield m.addGame(g);
+		
+		games = yield m.getAllGames();
+		assert(games.length == 2, 'should have two games played now');
+		
+		isComplete = yield m.isComplete();
+		assert(isComplete, 'match should be complete after one team wins two games');
 	});
 });
