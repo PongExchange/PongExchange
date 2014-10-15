e22f1f50b64927682e59656b6bd8f72d3a356c01
Amber Tunnell <amber.tunnell@gmail.com>
2014-10-15 15:31:29 -0400

    home page: adds recent games, top players. working on showing top teams as well.

--- src/web/controllers/HomeController.js
+++ src/web/controllers/HomeController.js
@@ -5,6 +5,7 @@
 
 var debug = require('neo-debug')('web.HomeController');
 var Player = require('models/Player');
+var Game = require('models/Game');
 
 /* =============================================================================
  * 
@@ -22,6 +23,15 @@ HomeController.indexGET = function * ()
 {
 	debug('Index page');
 
-	yield this.render('home/index');
+  var games = yield Game.getRecent(5);
+  var players = yield Player.getAllWithStats();
+
+  // sort by single games won
+  var singlePlayers = players.sort(function(p1, p2){ return p2.stats.singlesGamesWon-p1.stats.singlesGamesWon });
+  
+  // sort by double games won
+  var doublePlayers = players.sort(function(p1, p2){ return p2.stats.doublesGamesWon-p1.stats.doublesGamesWon });
+
+	yield this.render('home/index', { games: games, singlePlayers: singlePlayers.slice(0,5), doublePlayers: doublePlayers.slice(0,5)  });
 
 };
