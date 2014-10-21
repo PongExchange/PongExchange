6f5414ab5055ef54b270915cb696bce7956f33c7
Amber Tunnell <amber.tunnell@gmail.com>
2014-10-20 23:55:44 -0400

    games: can now add multiple games as matches

--- src/web/controllers/GamesController.js
+++ src/web/controllers/GamesController.js
@@ -6,6 +6,7 @@
 var debug = require('neo-debug')('web.GamesController');
 var Player = require('models/Player');
 var Game = require('models/Game');
+var Match = require('models/Match');
 
 /* =============================================================================
  * 
@@ -35,25 +36,32 @@ GamesController.newGET= function * ()
 GamesController.newPOST = function * ()
 {
 	var response = this.request.body.fields;
-	var g = new Game();
 
-	g.team1 = parseStringIdsToPlayers(response.team1.players);
-	g.team1Score = parseInt(response.team1.score);
+	var m = yield Match.newMatch();
 
-	g.team2 = parseStringIdsToPlayers(response.team2.players);
-	g.team2Score = parseInt(response.team2.score);
+	for (var i=0; i< response.team1.score.length; i++){
 
-	g.created_by_player_id = this.player.id;
-	
-	try
-	{
-		yield g.save();
+			var g = new Game();
+
+			g.team1 = parseStringIdsToPlayers(response.team1.players);
+			g.team1Score = parseInt(response.team1.score[i]);
+
+			g.team2 = parseStringIdsToPlayers(response.team2.players);
+			g.team2Score = parseInt(response.team2.score[i]);
+
+			g.created_by_player_id = this.player.id;
+			
+			try
+			{
+				yield g.save();
+				yield m.addGame(g);
+			}
+			catch (e)
+			{
+				// TODO: need to return the error message
+			}
 	}
-	catch (e)
-	{
-		// TODO: need to return the error message
-	}
-	
+
 	this.redirect('/');
 };
 
--- src/web/static/js/games.js
+++ src/web/static/js/games.js
@@ -1,5 +1,24 @@
 $(function(){
 
+ 
  $(".chosen-select").chosen({no_results_text: "Nobody found!", max_selected_options: 2}); 
+
+
+///////////// Adding score inputs for multiple games in a match.
+
+  var incrementer = function(){
+    var i = 1;
+    return function(){
+      return i+=1;
+    }
+   }
+  var inc = incrementer();
+
+ $("#add-game").click(function(event){
+   event.preventDefault();
+   var gameNum = inc();
+
+   $('#recordscores').append("<tr><th>game " + gameNum + "</th><td><input type=\'text\' name=\'team1[score][]\' placeholder=\'score\' class=\'form-control score\'></td><td><input type=\'text\' name=\'team2[score][]\' placeholder=\'score\' class=\'form-control score\'></td></tr>")
+ })
 
 });
